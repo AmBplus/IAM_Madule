32d2e50ef52dba08668386d6fb888b5ec027d82c
Amir <96792239+AmBplus@users.noreply.github.com>
2023-09-04 11:01:36 +0330

    feat
    .

---
+++ src/iam_api/Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.eShopOnContainers.Services.Identity.API.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+    }
+}
